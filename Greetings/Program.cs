60a0acced4c6bd993d973f891ce79945254fe401
Maria Spiridonova <miaiv@icloud.com>
2021-04-26 22:11:53 +0300

    Update the file Program.cs

--- Greetings/Program.cs
+++ Greetings/Program.cs
@@ -6,7 +6,11 @@ namespace Greetings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string myName; // Declaring a variable of type string
+            Console.WriteLine("Enter your name, please:"); // Log a message out
+            myName = Console.ReadLine(); // Store the input in the previously declared variable
+            Console.WriteLine("Nice to meet you, {0}!", myName); // Log the message to the console
+            Console.ReadLine();
         }
     }
 }
