5f149c71932052e03776e5d82a12dcb43b76c603
Maria Spiridonova <miaiv@icloud.com>
2021-04-26 23:26:22 +0300

    Creating a project 'Greetings'

---
+++ ITMO.CSProgrammingCourse/Lab1/ITMO.CSProgrammingCourse/Greetings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Greetings
+{
+    class Greeter
+    {
+        static void Main(string[] args)
+        {
+            string myName; // Declaring a variable of type string
+            Console.WriteLine("Enter your name, please:"); // Log the message to the console
+            myName = Console.ReadLine(); // Store the input in a variable declared above
+            Console.WriteLine("Nice to meet you, {0}!", myName); // Log the message to the console
+        }
+    }
+}
