eb1e3e49697445864831a70cea91a3aade55bc25
Maria Spiridonova <miaiv@icloud.com>
2021-05-04 20:59:34 +0300

    Started working on Lab 4

---
+++ Lab4/Utility/Utility/Test.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Utility
+{
+    using System;
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            int firstVariable, secondVariable, greater;
+
+            Console.WriteLine("Enter the first number please:");
+            firstVariable = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number please:");
+            secondVariable = int.Parse(Console.ReadLine());
+          
+            greater = Utils.Greater(firstVariable, secondVariable);
+            Console.WriteLine("The greater number is " + greater);
+           
+        }
+    }
+}
---
+++ Lab4/Utility/Utility/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility
+{
+    class Utils
+    {
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            } else
+            {
+                return b;
+            }
+        }
+    }
+}
