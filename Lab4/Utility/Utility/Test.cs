ccdd68a78bce045d432c1f21ee2100eff43bb56d
Maria Spiridonova <miaiv@icloud.com>
2021-05-04 22:38:23 +0300

    Added factorial and swap methods

--- Lab4/Utility/Utility/Test.cs
+++ Lab4/Utility/Utility/Test.cs
@@ -7,16 +7,39 @@ namespace Utility
     {
         static void Main(string[] args)
         {
-            int firstVariable, secondVariable, greater;
+            int firstVariable, secondVariable, greater; // Declare the variables needed for the code below to work
+            bool ok;
+            int factorial;
 
-            Console.WriteLine("Enter the first number please:");
-            firstVariable = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the second number please:");
-            secondVariable = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the first number please:"); // Ask user for input
+            firstVariable = int.Parse(Console.ReadLine()); // Store user's input in the variable declared above
+            Console.WriteLine("Enter the second number please:"); // Ask user to input another number
+            secondVariable = int.Parse(Console.ReadLine()); // Store user's input
           
-            greater = Utils.Greater(firstVariable, secondVariable);
-            Console.WriteLine("The greater number is " + greater);
-           
+            greater = Utils.Greater(firstVariable, secondVariable); // Use the Greater method from Utils class to find out which number's the greatest of the two
+            Console.WriteLine("The greater number is " + greater); // Log the comparison result to the console
+
+            // Test the swap method
+            Console.WriteLine("Before the swap the first number was {0} and the second one was {1}", firstVariable, secondVariable);
+            Utils.Swap(ref firstVariable, ref secondVariable); // Use the method from the Utils class to swap the values of the numbers
+            Console.WriteLine("After the swap the first number has a value of " + firstVariable + " and the second one is now " + secondVariable);
+
+            // Get factorial input
+            Console.WriteLine("Enter a number please: ");
+            int getFactorial = int.Parse(Console.ReadLine());
+
+            // Test the factorial function
+            ok = Utils.Factorial(getFactorial, out factorial);
+
+            if (ok) // Output results
+            {
+                Console.WriteLine("The factorial of {0} is {1}", getFactorial, factorial);
+            }
+            else
+            {
+                Console.WriteLine("Cannot calculate factorial of {0}", getFactorial);
+            }
+
         }
     }
 }
--- Lab4/Utility/Utility/Utils.cs
+++ Lab4/Utility/Utility/Utils.cs
@@ -6,7 +6,7 @@ namespace Utility
 {
     class Utils
     {
-        public static int Greater(int a, int b)
+        public static int Greater(int a, int b) // Return the greater of two intergers 
         {
             if (a > b)
             {
@@ -16,5 +16,47 @@ namespace Utility
                 return b;
             }
         }
+
+        public static void Swap(ref int a, ref int b) // Exchange the values of two integers, passed by reference
+        {
+            int temp = a; // Store the value of variable a in another variable
+            a = b; // Variable a now stores the value of variable b
+            b = temp; // Variable b now stores the initial value of variable a
+        }
+
+
+        // A method to calculate the factorial of a number
+        public static bool Factorial(int n, out int answer) // Return the result as an out parameter
+        {
+            int k; // Declare a local variable to serve as a counter
+            int f;
+            bool ok = true;
+
+            // Check the input value
+            if (n < 0)
+                ok = false;
+
+            try
+            {
+                checked
+                {
+                    f = 1;
+                    for (k = 2; k <= n; k++) // Use for-loop to calculate the factorial of n
+                    {
+                        f = f * k;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                f = 0;
+                ok = false;
+            }
+
+            answer = f; // Assign result value
+            return ok; // Return to caller
+
+        }
+
     }
 }
