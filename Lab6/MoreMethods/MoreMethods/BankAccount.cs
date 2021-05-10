cc49f5b737d7c3675a30b0f2a228d6338eaf5cae
Maria Spiridonova <miaiv@icloud.com>
2021-05-10 18:32:52 +0300

    Ex. 3 of Lab 6

---
+++ Lab6/MoreMethods/MoreMethods/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿
+class BankAccount
+{
+    public decimal Deposit(decimal amount)
+    {
+        if (amount > 0) 
+        {
+            accBal += amount;
+            return accBal;
+        } 
+        return accBal;
+    }
+    public bool Withdraw(decimal amount)
+    {
+        bool sufficientFunds = accBal >= amount;
+        if (sufficientFunds)
+        {
+            accBal -= amount;
+        }
+        return sufficientFunds;
+    }
+    public void Populate(decimal balance)
+    {
+        accNo = NextNumber();
+        accBal = balance;
+        accType = AccountType.Checking;
+    }
+
+    public long Number()
+    {
+        return accNo;
+    }
+
+    public decimal Balance()
+    {
+        return accBal;
+    }
+
+    public string Type()
+    {
+        return accType.ToString();
+    }
+
+    private long accNo;
+    private decimal accBal;
+    private AccountType accType;
+
+    private static long nextNumber = 123;
+
+    private static long NextNumber()
+    {
+        return nextNumber++;
+    }
+}
