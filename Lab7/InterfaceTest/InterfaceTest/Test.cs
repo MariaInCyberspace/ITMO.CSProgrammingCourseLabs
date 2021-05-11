389d983fbbe9c0f2b5e9882bc00118434a751292
Maria Spiridonova <miaiv@icloud.com>
2021-05-11 04:36:33 +0300

    Ex. 4 of Lab 7

---
+++ Lab7/InterfaceTest/InterfaceTest/Test.cs
@@ -0,0 +1,23 @@
+﻿namespace Utils
+{
+    using System;
+
+    /// <summary>
+    ///   This the test harness
+    /// </summary>
+
+    public class Test
+    {
+        public static void Main()
+        {
+            int i = 0;
+            ulong u = 0;
+            string s = "Test";
+
+            Console.WriteLine("int: {0}", Utils.IsItFormattable(i));
+            Console.WriteLine("ulong: {0}", Utils.IsItFormattable(u));
+            Console.WriteLine("String: {0}", Utils.IsItFormattable(s));
+
+        }
+    }
+}
