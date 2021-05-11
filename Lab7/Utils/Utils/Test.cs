c1cd9286e2d51593220db6adfef910b80e144d3f
Maria Spiridonova <miaiv@icloud.com>
2021-05-11 03:47:44 +0300

    Ex.2 of Lab 7

---
+++ Lab7/Utils/Utils/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Utils
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            string someText;
+            Console.WriteLine("Type some text please: "); // Ask for input
+            someText = Console.ReadLine(); // Store the input in a variable of type string
+            
+            // Reverse the string 
+            Utils.Reverse(ref someText);
+
+            // Log the result to the console
+            Console.WriteLine(someText);
+        }
+    }
+}
