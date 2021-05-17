dbc12ee1ed5f3bdd3933e630907b9b0e68686537
Maria Spiridonova <miaiv@icloud.com>
2021-05-17 20:47:15 +0300

    Ex. 1 of Lab 1

--- Lab1/Greetings/Program.cs
+++ Lab1/Greetings/Program.cs
@@ -7,7 +7,7 @@ namespace Greetings
         static void Main(string[] args)
         {
             Console.WriteLine("Enter your name, please:"); // Log the message to the console
-            string myName = Console.ReadLine(); // Declaring a variable of type string and store the input in a variable declared above
+            string myName = Console.ReadLine(); // Declaring a variable of type string and store the input in it
             Console.WriteLine("Nice to meet you, {0}!", myName); // Log the message to the console
         }
     }
