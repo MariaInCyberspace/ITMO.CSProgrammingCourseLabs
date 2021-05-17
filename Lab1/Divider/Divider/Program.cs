c04353d621475c35504620035b4c40943e4bf908
Maria Spiridonova <miaiv@icloud.com>
2021-05-17 19:12:44 +0300

    Ex.2 of Lab 1 - Divider

--- Lab1/Divider/Divider/Program.cs
+++ Lab1/Divider/Divider/Program.cs
@@ -6,20 +6,19 @@ namespace Divider
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the first number, please:"); 
+            int firstNumber = int.Parse(Console.ReadLine()); // Store the input in the previously declared variable
+            Console.WriteLine("Enter the second number, please:");
+            int secondNumber = int.Parse(Console.ReadLine()); // Store the input
+
             try 
             {
-                // Declaring variables of type integer
-                int firstNumber, secondNumber;
-                Console.WriteLine("Enter the first number, please:"); 
-                firstNumber = int.Parse(Console.ReadLine()); // Store the input in the previously declared variable
-                Console.WriteLine("Enter the second number, please:");
-                secondNumber = int.Parse(Console.ReadLine()); // Store the input
-                int divided = firstNumber / secondNumber; // Store the result of the first number divided by the second number in a new variable
-                Console.WriteLine("{0} divided by {1} is {2}.", firstNumber, secondNumber, divided); 
-            } 
-            catch(Exception e)
+            int divided = firstNumber / secondNumber; // Store the result of the first number divided by the second number in a new variable
+            Console.WriteLine("{0} divided by {1} is {2}.", firstNumber, secondNumber, divided);
+            }
+            catch (DivideByZeroException e) 
             {
-                Console.WriteLine("An exception was thrown: {0}", e);
+            Console.WriteLine("An exception was thrown: {0}", e);
             }
             
         }
